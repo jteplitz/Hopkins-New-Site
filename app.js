c99e36848c5e4a8ab449addd89fdbd2278cdf2fb
Jason Teplitz <Jason@Jason-Teplitzs-Mac.local>
2012-02-04 15:17:27 -0500

    heroku compatabile

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3100);
+app.listen(process.env.PORT || 3100);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
